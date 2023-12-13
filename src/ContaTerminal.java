61c1d8452c2c84ccff1604a8d8c682c593b8801f
DjalmaJunior <djalmajunior@spassu.com.br>
2023-12-13 08:36:21 -0300

    Concluindo o Projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner inserirDados = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = inserirDados.nextLine();
+        System.out.println("Usuário: ");
+        int usuario = inserirDados.nextInt();
+        inserirDados.nextLine();
+        System.out.println("Nome : ");
+        String name = inserirDados.nextLine();
+        System.out.println("Saldo : ");
+        String saldo = inserirDados.nextLine();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Olá " + name + ", Obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + usuario + " e seu saldo " + saldo + " já está disponivel para saque");
+
+
     }
 }
